ed5d1bf0e40c70519fe32c372777946602ae0a46
Mandeep Singh <mandeep@Mandeeps-MacBook-Air.local>
2025-09-30 10:29:01 +0530

    first commit

---
+++ Day2/VariablesDemo.ts
@@ -0,0 +1,13 @@
+console.log("Welcome to TypeScript");
+console.log("This is Day 2 - Variables in TypeScript");
+var age:number=30;
+var firstName:string="Mandeep";
+console.log("My name is "+firstName+" and I am "+age+" years old");
+let box;
+console.log(typeof(box)); // undefined
+
+box = "Hello";
+console.log(typeof(box)); // string
+
+box = 100;
+console.log(typeof(box)); // number
