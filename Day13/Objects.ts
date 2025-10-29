0819bffcf8406813b30d0327ee336937c569045b
mandeepleo <mandeepleo5@gmail.com>
2025-10-29 10:53:47 +0530

    resolve conflict 2

--- Day13/Objects.ts
+++ Day13/Objects.ts
@@ -3,7 +3,4 @@
 // 2. Inline Type Object - We also define the datatype of the keys (TS)
 // 3. Using type aliases (JS/TS)
 // 4. Using the classes (JS ES16/TS)
-<<<<<<< HEAD
 // 5.
->>>>>>> ts-rsys
-
