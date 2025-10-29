68f9c5c6ac72ad8b427af6ef24915fc44f90aa97
mansingh <mansingh@radisys.com>
2025-10-29 10:12:09 +0530

    added Objects.ts

---
+++ Day13/Objects.ts
@@ -0,0 +1,6 @@
+// Different ways to create an oject in JS/TS //
+// 1. using 'object' type - Directly define the values for variable (JS/TS)
+// 2. Inline Type Object - We also define the datatype of the keys (TS)
+// 3. Using type aliases (JS/TS)
+// 4. Using the classes (JS ES16/TS)
+
