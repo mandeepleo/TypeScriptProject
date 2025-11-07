742ce6108263ca17a89487a99d2d0fea877f5549
mansingh <mansingh@radisys.com>
2025-11-07 11:15:29 +0530

    added day16

---
+++ Day16/Interface.ts
@@ -0,0 +1,6 @@
+/** Interface in TypeScript is a way to define the structure of an object.
+ * It tells the compiler what properties and methods an object should have.
+ * Interfaces are used to enforce certain contracts within your code and to provide better type-checking.
+ * They can be implemented by classes or used to type-check objects.
+ **/
+ 
